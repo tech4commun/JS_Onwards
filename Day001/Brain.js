e3d59809b39a2c77f027ea79202866371ec3a7ab
tech4commun <pedaltriangle@gmail.com>
2025-10-17 23:01:49 +0530

    Add Brain.js with initial JavaScript examples and explanations

---
+++ Day001/Brain.js
@@ -0,0 +1,27 @@
+console.log("hello");
+
+let num=10;
+console.log(num)
+
+let ar ="arpit"; //ar variable
+ar ="arpit verma"
+console.log(ar)
+
+//const
+const ids =0.999999;
+console.log(ids)
+//ids=30; // it will give error immutable 
+
+//let vs const
+/**let- value is mutable
+ * const means constant the value never be changed it cannot be change
+*/
+
+//var
+/**
+ * var is old method of let-it has some big disadvantage
+ * var is also mutable
+ */
+var a =30;
+a=40;
+console.log(a)
